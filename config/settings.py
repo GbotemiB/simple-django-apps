7ee4d558ac650c10e094d32ab38c87f67f112904
GbotemiB <gbotemibolarinwa@gmail.com>
2021-04-27 20:57:33 +0000

    password config added

--- config/settings.py
+++ config/settings.py
@@ -128,3 +128,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
